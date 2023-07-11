a8b7f8eda1e669e34759f9850a16bbafbecde609
Talha Erdoğan <45467637+Talha-Erdogan@users.noreply.github.com>
2023-07-11 08:46:57 +0300

    tema ekleme

---
+++ HealthyLifeStyle.Web/Controllers/HospitalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyLifeStyle.Web.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
